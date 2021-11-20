2ba43607992acc264bb50945e73672c5bb79e936
Jim Yoon <jimyoon90@gmail.com>
2021-11-19 19:35:00 -0800

    add node runtime to test file

--- medium/addTwoNumbers.js
+++ medium/addTwoNumbers.js
@@ -2,7 +2,6 @@
 // You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
 // You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
-
 // Definition for singly-linked list.
 function ListNode(val) {
   this.val = val;
--- test.js
+++ test.js
@@ -1,1 +1,4 @@
 console.log('This is the start of testing an algorithm');
+console.time('test runtime');
+
+console.timeEnd('test runtime');
